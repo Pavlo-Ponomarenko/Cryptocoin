6a5f8e0737a242e06dec8b934a583ee8fb3f595b
Pavlo Ponomarenko <reactor2345@gmail.com>
2023-02-07 10:01:37 +0200

    Initial commit

---
+++ src/main/java/org/blockchain/App.java
@@ -0,0 +1,13 @@
+package org.blockchain;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
