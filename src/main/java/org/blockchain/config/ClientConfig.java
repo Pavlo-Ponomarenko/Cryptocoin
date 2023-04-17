0a54b8e54cea561910b645bd42991cf9d3ead7d2
Pavlo Ponomarenko <reactor2345@gmail.com>
2023-04-17 19:01:31 +0300

    Reading clients addresses from app arguments

---
+++ src/main/java/org/blockchain/config/ClientConfig.java
@@ -0,0 +1,19 @@
+package org.blockchain.config;
+
+import org.blockchain.network.Client;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+public class ClientConfig {
+
+    @Value("${addresses}")
+    private String otherClientsAddresses;
+
+    public List<Client> getOtherClients() {
+        return Arrays.stream(otherClientsAddresses.split(",")).map(Client::new).toList();
+    }
+}
--- src/main/java/org/blockchain/repository/TransactionRepository.java
+++ src/main/java/org/blockchain/repository/TransactionRepository.java
@@ -3,5 +3,5 @@ package org.blockchain.repository;
 import org.blockchain.entities.TransactionRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TransationRepository extends JpaRepository<TransactionRecord, Long> {
+public interface TransactionRepository extends JpaRepository<TransactionRecord, Long> {
 }
