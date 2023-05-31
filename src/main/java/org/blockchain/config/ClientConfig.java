3423821d8330bf7b2d25bd99f2d914279f4aa170
Pavlo Ponomarenko <reactor2345@gmail.com>
2023-05-31 11:28:00 +0300

    Input parameters processing fix

--- src/main/java/org/blockchain/config/ClientConfig.java
+++ src/main/java/org/blockchain/config/ClientConfig.java
@@ -1,23 +1,22 @@
 package org.blockchain.config;
 
 import org.blockchain.client.Client;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 @Configuration
-public class ClientConfig {
+public class ClientConfig implements CommandLineRunner {
 
     public static final int transactionsInBlock = 3;
     public static final String mining_difficulty = "00000FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF";
 
-    @Value("${addresses}")
-    private String otherClientsAddresses;
+    private List<Client> clients;
 
     @Bean
     public ExecutorService executorService() {
@@ -25,6 +24,14 @@ public class ClientConfig {
     }
 
     public List<Client> getOtherClients() {
-        return Arrays.stream(otherClientsAddresses.split(",")).map(Client::new).toList();
+        return clients;
+    }
+
+    @Override
+    public void run(String... args) {
+        clients = new ArrayList<>();
+        for (String address : args) {
+            clients.add(new Client(address));
+        }
     }
 }
